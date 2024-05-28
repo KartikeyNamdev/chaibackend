4c4a4bfe680b7dd6a5bb40579460dde8650cd052
KartikeyNamdev <kartikeynamdev2003@gmail.com>
2024-05-28 12:49:29 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const { request } = require("express");
+
+console.log("Hello kar");
+
+const express = require("express"); //Creates an object from dependency express and storing in an object
+
+const app = express();
+
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello Kartikey");
+});
+app.get("/login", (req, res) => {
+  res.send("<h1>Please login at chai and code</h1>");
+});
+app.get("/youtube", (req, res) => {
+  res.send("<h2>This is youtube, chai and code</h2>");
+});
+
+app.get("/page2", (req, res) => {
+  res.send("This is page 2");
+});
+
+app.listen(process.env.port, () => {
+  console.log("This server is working on localhost 3000");
+});
